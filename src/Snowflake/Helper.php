497ad4b0362d57b2cd544e39532400aab3ab3d11
Ondra <ondra.jodas@gmail.com>
2023-02-01 00:24:36 +0100

    wip

--- src/Snowflake/Helper.php
+++ src/Snowflake/Helper.php
@@ -54,10 +54,9 @@ class Helper
     public static function generateRandomString(int $length = 10): string
     {
         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $charactersLength = strlen($characters);
         $randomString = '';
         for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[rand(0, $charactersLength - 1)];
+            $randomString .= $characters[random_int(0, strlen($characters) - 1)];
         }
         return $randomString;
     }
