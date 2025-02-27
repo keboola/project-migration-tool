4599775304522dbd3bb8da0ae81d06479e1d1ee7
Ondřej Jodas <ondra.jodas@gmail.com>
2025-02-27 08:40:44 +0100

    More logging

--- src/Cleanup.php
+++ src/Cleanup.php
@@ -201,7 +201,11 @@ class Cleanup
 
             // Drop users owned by roles
             if (!empty($dataToRemove[self::USER])) {
-                $this->logger->info(sprintf('Dropping %d users', count($dataToRemove[self::USER])));
+                $this->logger->info(sprintf('Dropping %d users: ', count($dataToRemove[self::USER])));
+                $this->logger->info(sprintf(implode(
+                    ', ',
+                    array_column($dataToRemove[self::USER], 'name')
+                )));
             }
             foreach ($dataToRemove[self::USER] as $user) {
                 [$sqls, $currentRole] = $this->switchRole($user['granted_by'], $mainRoleName, $sqls, $currentRole);
@@ -213,7 +217,11 @@ class Cleanup
 
             // Drop roles
             if (!empty($dataToRemove[self::ROLE])) {
-                $this->logger->info(sprintf('Dropping %d roles', count($dataToRemove[self::ROLE])));
+                $this->logger->info(sprintf('Dropping %d roles: ', count($dataToRemove[self::ROLE])));
+                $this->logger->info(sprintf(implode(
+                    ', ',
+                    array_column($dataToRemove[self::ROLE], 'name')
+                )));
             }
             foreach ($dataToRemove[self::ROLE] as $role) {
                 [$sqls, $currentRole] = $this->switchRole($role['granted_by'], $mainRoleName, $sqls, $currentRole);
